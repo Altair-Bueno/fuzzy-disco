be1cf1b00fc33892bb1b84f2f02854fcbf210427
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2021-09-07 19:38:57 +0200

    new: User update
    fix: docker-compose.yaml mongo didn't have an admin account
    
    ref: python test now have separate functions

--- disco-core/src/api/users/data.rs
+++ disco-core/src/api/users/data.rs
@@ -4,3 +4,7 @@ pub struct UpdatePassword<'a> {
     pub new_password: &'a str,
     pub password: &'a str,
 }
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UpdateUser<'a> {
+    pub email: Option<&'a str>,
+}
--- disco-core/tests/test.py
+++ disco-core/tests/test.py
@@ -1,10 +1,8 @@
 from users import test_api_users
 
-_URL = 'http://127.0.0.1:8000'
-
 
 def main():
-    test_api_users(_URL)
+    test_api_users()
 
 
 if __name__ == '__main__':
