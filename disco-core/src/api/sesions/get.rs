2b48ee9cc5f0fe9d7f36595b562402b519229e5b
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2021-09-08 14:46:10 +0200

    doc: Added /api/sesions/ doc

--- disco-core/src/api/sesions/get.rs
+++ disco-core/src/api/sesions/get.rs
@@ -9,6 +9,42 @@ use crate::api::result::ApiError;
 use std::collections::HashMap;
 use crate::api::sesions::data::PublicSesionData;
 
+/// # AUTH! `GET /api/sesions`
+/// Returns all current sesions from the user
+///
+/// # Returns
+/// ## Ok (200)
+///
+/// ```json
+/// {
+///     "ip": String,
+///     "date": String
+/// }
+/// ```
+///
+/// ## Err
+/// ```json
+/// {
+///     "status": String,
+///     "message": String
+/// }
+/// ```
+///
+/// | Code | Description |
+/// | -----| ----------- |
+/// | 500 | Couldn't connect to database |
+///
+/// # Example
+///
+/// `GET /api/sesions`
+///
+/// ```json
+/// {
+///     "ip": "127.0.0.1",
+///     "date": "2021-09-08 12:36:51.077 UTC"
+/// }
+/// ```
+
 #[get("/", format = "json")]
 pub async fn get_user_sesions(session_collection: &State<Collection<Sesion>>,token: TokenClaims) -> Result<Json<Vec<PublicSesionData>>, ApiError> {
     let filter = doc! { "user_alias" : token.alias().to_string() };
--- disco-core/src/api/sesions/post.rs
+++ disco-core/src/api/sesions/post.rs
@@ -5,6 +5,33 @@ use rocket::State;
 use crate::api::sesions::delete_all_sesions_from;
 use crate::api::result::ApiError;
 
+/// # AUTH! `POST /api/sesions/delete`
+///
+/// Deletes all sesions from the current user, included the current one. Can be
+/// used to log out on all browsers, for example
+///
+/// > Note: This is a no body post request, with no body response
+///
+/// # Returns
+/// ## Ok (204)
+///
+///
+/// ## Err
+/// ```json
+/// {
+///     "status": String,
+///     "message": String
+/// }
+/// ```
+///
+/// | Code | Description |
+/// | -----| ----------- |
+/// | 500 | Couldn't connect to database |
+///
+/// # Example
+///
+/// `POST /api/sesions/delete`
+
 #[post("/delete", data="<body>")]
 pub async fn delete_all_sessions(
     sesion_collection: &State<Collection<Sesion>>,
