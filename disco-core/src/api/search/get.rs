1fb91f1bfefaa828fd85eff2acac8d017002a116
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2021-09-16 21:34:55 +0200

    BREAKING CHANGE!: Search allows custom responses

--- disco-core/src/api/search/get.rs
+++ disco-core/src/api/search/get.rs
@@ -16,16 +16,18 @@ use crate::api::result::ApiResult;
 use crate::api::{POSTS_CREATION_DATE, USER_CREATION_DATE};
 use crate::mongo::post::Post;
 use crate::mongo::user::User;
+use crate::api::search::data::Window;
 
-/// # `GET /api/search?s=<string>&drop=<usize>&get=<u8>&date=<string>`
+/// # `GET /api/search?s=<string>&date=<string>&user.drop=<usize>&user.get=<u8>&post.drop=<usize>&post.get=<u8>`
 /// Returns matches for the given search string. The method receives the
 /// following query parameters
 ///
 /// - `s`: Search string
-/// - `drop`: Number of posts/users we want to skip. This avoids repetition on
-/// future queries
-/// - `get`: Number of posts we want to retrieve. The max is 255 posts
 /// - `date`: JSON formatted date, from where to start the query
+/// - `drop`: Number of posts/users we want to skip. This avoids repetition on
+/// future queries. Defaults to 0 if no value is provided
+/// - `get`: Number of posts/users we want to retrieve. The max is 255 elements.
+/// Defaults to 0 if no value is provided
 ///
 /// # Returns
 /// ## Ok(200)
@@ -50,40 +52,49 @@ use crate::mongo::user::User;
 /// | 400 | Bad request |
 /// | 404 | User doesn't exist |
 /// | 500 | Couldn't connect to database |
-#[get("/?<s>&<drop>&<get>&<date>")]
+#[get("/?<s>&<user>&<post>&<date>")]
 pub async fn search(
     s: &str,
-    drop: usize,
-    get: u8,
+    user:Window,
+    post:Window,
     date: ApiDate,
     user_collection: &State<Collection<User>>,
     posts_collection: &State<Collection<Post>>,
 ) -> ApiResult<Json<Value>> {
     let date = date.extract();
-    let filter_users = vec![
-        doc! { "$match": {
-            "$text": {"$search": s},
-            USER_CREATION_DATE:{ "$lte": date }
-        }},
-        doc! { "$sort": { "score": { "$meta": "textScore" } } },
-        doc! { "$skip": to_bson(&drop).unwrap() },
-        doc! { "$limit": to_bson(&get).unwrap() },
-    ];
-    let filter_posts = vec![
-        doc! { "$match": {
-            "$text": {"$search": s},
-            POSTS_CREATION_DATE:{ "$lte": date }
-        }},
-        doc! { "$sort": { "score": { "$meta": "textScore" } } },
-        doc! { "$skip": to_bson(&drop).unwrap() },
-        doc! { "$limit": to_bson(&get).unwrap() },
-    ];
-    // TODO async search on parallel using `sope` threads
+
+    // TODO async search on parallel using `scope` threads
     // Note: AsyncDrop does not exist, neither scoped async tasks. It is not
     // currently possible to perform two separate queries on different tasks
     // without wrapping the client on `Ark`
-    let users: Vec<ApiUserResponse> = search_on_collection(filter_users, user_collection).await?;
-    let posts: Vec<ApiPostResponse> = search_on_collection(filter_posts, posts_collection).await?;
+    let users: Option<Vec<ApiUserResponse>> = if user.get >0 {
+        let filter_users = vec![
+            doc! { "$match": {
+            "$text": {"$search": s},
+            USER_CREATION_DATE:{ "$lte": date }
+        }},
+            doc! { "$sort": { "score": { "$meta": "textScore" } } },
+            doc! { "$skip": to_bson(&user.drop).unwrap() },
+            doc! { "$limit": to_bson(&user.get).unwrap() },
+        ];
+        Some(search_on_collection(filter_users, user_collection).await?)
+    } else {
+        None
+    };
+    let posts: Option<Vec<ApiPostResponse>> = if post.get > 0 {
+        let filter_posts = vec![
+            doc! { "$match": {
+            "$text": {"$search": s},
+            POSTS_CREATION_DATE:{ "$lte": date }
+        }},
+            doc! { "$sort": { "score": { "$meta": "textScore" } } },
+            doc! { "$skip": to_bson(&post.drop).unwrap() },
+            doc! { "$limit": to_bson(&post.get).unwrap() },
+        ];
+        Some(search_on_collection(filter_posts, posts_collection).await?)
+    } else {
+        None
+    };
     Ok(Json(json!({"users":users,"posts":posts})))
 }
 
--- disco-core/tests/search.py
+++ disco-core/tests/search.py
@@ -9,6 +9,6 @@ _basic_header = {
 _URL = 'http://127.0.0.1:8000/api/search'
 
 
-def search(s: str, drop: int, get: int, date: str):
+def search(s: str,date: str,post_drop=0,post_get=0, user_drop=0, user_get=0):
     return requests.get(
-        _URL + f'?s={s}&drop={drop}&get={get}&date={urllib.parse.quote(date)}')
+        _URL + f'?s={s}&user.drop={user_drop}&user.get={user_get}&post.drop={post_drop}&post.get={post_get}&date={urllib.parse.quote(date)}')
