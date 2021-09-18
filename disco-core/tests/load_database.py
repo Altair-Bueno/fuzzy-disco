e91710425fb676565ed76fe04129ddcabcc460c1
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2021-09-18 21:37:42 +0200

    new: Scripts for load testing the API

--- disco-core/tests/load_database.py
+++ disco-core/tests/load_database.py
@@ -1,22 +1,54 @@
-import random
-
+import media
 import users
 import payloads
 import post
+import os
+import random
 from concurrent.futures import ThreadPoolExecutor
 
 
-def create_dummy_user(i:int):
-    body = payloads.new_user(f"dummy{i}",f"dummy{i}@hello.com","12341234")
-    return users.create_user(body)
+def get_random_audio():
+    return get_random_file("resources/audio")
 
 
-def generate_data():
+def get_random_image():
+    return get_random_file("resources/images")
+
+
+def get_random_file(path:str):
+    file = random.choice(os.listdir(path))
+    return f"{path}/{file}"
+
+
+def load(id:int):
+    body = payloads.new_user(f"hello{id}", f"a{id}@uma.es", "12341234")
+    r = users.create_user(body)
+    if not r.ok:
+        print(r.text)
+        return -1
+    login = payloads.login_alias(f"hello{id}", "12341234")
+    r = users.alias_log_in(login)
+    auth_header = payloads.auth_header(r.json()["access_token"])
+    refresh_token = r.json()["refresh_token"]
+    for i in range(0, 3):
+        audio = get_random_audio()
+        image = get_random_image()
+        id_audio = media.upload_media(audio,auth_header).json()["key"]
+        id_image = media.upload_media(image,auth_header).json()["key"]
+        body = payloads.new_post(f"Sample{id}",f"Test caption {audio} and {image}",id_image,id_audio,payloads.VISIBILITY_PUBLIC)
+        post.create_post(body,auth_header)
+    return id
+
+
+def main():
     with ThreadPoolExecutor() as executor:
-        running_tasks = [executor.submit(lambda: create_dummy_user(i)) for i in range(0,10000)]
-        for running in running_tasks:
-            print(running.result().json())
+        l = []
+        for i in range(0,40):
+            r = executor.submit(lambda n=i: load(n))
+            l.append(r)
+        for wait in l:
+            print(wait.result())
 
 
 if __name__ == "__main__":
-    generate_data()
+    main()
